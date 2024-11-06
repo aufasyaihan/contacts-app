d169e72b09445926bdbaf1919a44dc44771d9854
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-11-06 10:07:14 +0700

    added delete handler

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import peopleType from "../types/people";
 import ContactItem from "./ContactItem";
 
 function ContactList({ contacts }: { contacts: peopleType[] }) {
+  const [people, setPeople] = useState<peopleType[]>(contacts);
+  const onDelete = (id: number) => {
+    setPeople((prevState) => prevState.filter((people) => people.id !== id));
+  };
   return (
     <div className="contact-list">
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} {...contact} />
+      {people.map((people) => (
+        <ContactItem key={people.id} onDelete={onDelete} {...people} />
       ))}
     </div>
   );
