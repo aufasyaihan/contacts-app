474aefb19bf6b317ce259cbba1a6696f573307d4
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-11-06 10:06:39 +0700

    added delete button to contact item

--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -1,19 +1,28 @@
 import ContactItemBody from "./ContactItemBody";
 import ContactItemImage from "./ContactItemImage";
+import DeleteButton from "./DeleteButton";
 
-function ContactItem({
-  imageUrl,
-  name,
-  tag,
-}: {
+interface ContactItemProps {
+  id: number;
   imageUrl: string;
   name: string;
   tag: string;
-}) {
+  onDelete: (id: number) => void;
+}
+
+function ContactItem({
+  id,
+  imageUrl,
+  name,
+  tag,
+  onDelete
+}: ContactItemProps) {
+  
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete}/>
     </div>
   );
 }
